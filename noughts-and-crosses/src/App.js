e0142fbd0baee2b05227e5c98272d0776d75b43a
jack dachtler <dachtler@live.co.uk>
2022-02-13 21:23:03 +0000

    useState homework

--- noughts-and-crosses/src/App.js
+++ noughts-and-crosses/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import React from 'react';
-import Header from './components/header';
-import Grid from './components/grid';
+import "./App.css";
+import React from "react";
+import Header from "./components/header";
+import Grid from "./components/grid";
 
 function App() {
+  const grid = [];
+
+  for (let i = 0; i < 9; i++) {
+    grid.push({});
+  }
   return (
     <>
       <Header />
-      <Grid />
+      <Grid grid={grid} />
     </>
   );
 }
--- noughts-and-crosses/src/components/grid.js
+++ noughts-and-crosses/src/components/grid.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Grid() {
-    /* TODO: Pass the grid in as a prop */
-    const grid = [];
+function Grid(props) {
+  const Players = () => {
+    const [turnX, setTurnX] = useState("X");
+    const [turnO, setTurnO] = useState("O");
 
-    for (let i = 0; i < 9; i++) {
-        grid.push({});
-    }
+    //const onclick = () => {
+    const turnX = "X";
+    const turnO = "O";
+    setTurnX(turnO);
+    setTurnO(turnX);
+  };
 
-    return (
-        <div className="grid">
-            <div className="grid-inner">
-                {/* TODO: 
+  console.log(Players);
+
+  return (
+    <div className="grid">
+      <div className="grid-inner">
+        {/* TODO: 
                     1. When I click on a box I want to see a message in the console.
-                    2. When I click on a box I want to see a number in the console of the box. */}
-                {grid.map(item => (<div className="box"></div>))}
-            </div>
-        </div>
-    );
+                    2. When I click on a box I want to see a number in the console of the box. 
+                    3. First time i click on a box, in the console i want see and X, second time I want to see a 0, and repeat pattern... 
+                    In the grid you will need a local state i.e. useState.
+                    4. Prepare talk on all types of functions*/}
+
+        {props.grid.map((item, idx) => (
+          <div className="box" onClick={(setTurnX) => setTurnX(turnX)}>
+            <div className="box" onClick={(setTurnO) => setTurnO(turnO)}></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Grid;
