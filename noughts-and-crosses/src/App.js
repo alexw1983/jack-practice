210477c930a1e4b45ff796bbf282c781b4ebdee4
Alex Wilson <alex.adwilson@gmail.com>
2022-02-22 13:53:19 +0000

    added some notes

--- noughts-and-crosses/src/App.js
+++ noughts-and-crosses/src/App.js
@@ -22,6 +22,24 @@ const getInitialGridState = () => {
   return grid;
 }
 
+/*
+* TODO:
+*   - move everything up to the APP component from grid
+*   - only pass the grid to the grid component as a prop
+*   - pass in an onclick style function to the grid component as a prop (just log to console for now). When you click on a box something should appear in the console.
+----------------------
+*   - move the grid array in the app component in to state on the app component.
+*   - you will need to give the useState() function an initial value. 
+*   - This should be an array of 9 objects with approppriate properties.
+*   - Have the onclick handler in the App component 
+*       - (i.e. the function that is called when you click on a box change the state setting the value to the current Turn)
+*       - because we are passing the grid in as a prop it should re-render the component.
+*       - you will also need to switch turns as we have already done.
+----------------------
+*   - Optional
+*     - show the current turn in the header.
+*/
+
 function App() {
   const [currentTurn, setCurrentTurn] = useState("X");
   const [grid, setGrid] = useState(getInitialGridState());
