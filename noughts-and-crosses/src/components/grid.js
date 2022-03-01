09fb0032399228748a9c2f3241318351b09dc078
jack dachtler <dachtler@live.co.uk>
2022-03-01 21:22:41 +0000

    x and o appear in grid

--- noughts-and-crosses/src/components/grid.js
+++ noughts-and-crosses/src/components/grid.js
@@ -1,20 +1,15 @@
 import React from "react";
 
 function Grid(props) {
-
-    return (
-        <div className="grid">
-            <div className="grid-inner">
-        {/* TODO: 
-                    1. When I click on a box I want to see a message in the console.
-                    2. When I click on a box I want to see a number in the console of the box. 
-                    3. First time i click on a box, in the console i want see and X, second time I want to see a 0, and repeat pattern... 
-                    In the grid you will need a local state i.e. useState.
-                    4. Prepare talk on all types of functions
-                    5. Get the X or O to appear opn the page*/}
-
+  return (
+    <div className="grid">
+      <div className="grid-inner">
         {props.grid.map((item, idx) => (
-          <div className="box" onClick={() => props.onBoxClick(item.position)} key={`box_${idx}`}>
+          <div
+            className="box"
+            onClick={() => props.onBoxClick(item.position)}
+            key={`box_${idx}`}
+          >
             <p>{item.value}</p>
           </div>
         ))}
--- noughts-and-crosses/src/components/header.js
+++ noughts-and-crosses/src/components/header.js
@@ -4,7 +4,7 @@ function Header(props) {
     return (
         <div className="center-screen" >
             <h1>Noughts and Crosses</h1>
-            <h3> CurrentTurn = {props.currentTurn}</h3>
+            <h3> The Current Turn is {props.currentTurn}</h3>
             <hr />
         </div>
     );
